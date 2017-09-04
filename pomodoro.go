240edfc84e412428c722bce585a11319983f3e38
fynex <fynex@example.com>
2017-09-04 22:14:06 +0200

    created a state

--- pomodoro.go
+++ pomodoro.go
@@ -16,7 +16,12 @@ var l = flag.Int("l", 15, "Duration of a long break")
 var p = flag.String("p", "wswswl", "Pattern to  follow (for example wswswl)")
 
 
-func Notify(summary string, body string, isUrgent bool) {
+type Pomodoro struct { 
+    curr_state string
+}
+
+
+func (p *Pomodoro) Notify(summary string, body string, isUrgent bool) {
     if isUrgent {
         exec.Command("notify-send", summary, body, "-u", "critical").Run()
     } else {
@@ -26,8 +31,18 @@ func Notify(summary string, body string, isUrgent bool) {
 }
 
 
-func Work(duration *int) {
-    Notify(HEADLINE, "Start Working!", false)
+func (p *Pomodoro) Work(duration *int, num int) {
+    p.Notify(HEADLINE, "Start Working!", false)
+
+    timer1 := time.NewTimer(time.Minute * time.Duration(*duration))
+
+    <-timer1.C
+    fmt.Printf("    ☑ %d. Working phase\n", num)
+}
+
+
+func (p *Pomodoro) ShortBreak(duration *int) {
+    p.Notify(HEADLINE, "Take a BREAK", true)
 
     timer1 := time.NewTimer(time.Minute * time.Duration(*duration))
 
@@ -35,27 +50,15 @@ func Work(duration *int) {
 }
 
 
-func ShortBreak(duration *int) {
-    Notify(HEADLINE, "Take a BREAK", true)
+func (p *Pomodoro) LongBreak (duration *int) {
+    p.Notify(HEADLINE, "Take a long BREAK", true)
 
     timer1 := time.NewTimer(time.Minute * time.Duration(*duration))
 
     <-timer1.C
 }
 
-
-func LongBreak (duration *int) {
-    Notify(HEADLINE, "Take a long BREAK", true)
-
-    timer1 := time.NewTimer(time.Minute * time.Duration(*duration))
-
-    <-timer1.C
-}
-
-
-func main() {
-    flag.Parse()
-
+func (pom *Pomodoro) PrintHeader() {
     fmt.Println(".===========================.")
     fmt.Println("|          Pomodoro         |")
     fmt.Println("'==========================='")
@@ -63,19 +66,35 @@ func main() {
     fmt.Printf("  * Break      : %d minutes\n", *s)
     fmt.Printf("  * Long Break : %d minutes\n", *l)
     fmt.Printf("  * Working Set: %s \n", *p)
+    fmt.Println("\n  [*] Status ")
+}
+
+func (pom *Pomodoro) Run() {
+    i := 1
 
     for _,state := range *p {
-        curr_state := string(state)
+        pom.curr_state = string(state)
 
-        switch curr_state {
+        switch pom.curr_state {
         case "w":
-            Work(w)
+            pom.Work(w, i)
+            i += 1
         case "s":
-            ShortBreak(s)
+            pom.ShortBreak(s)
         case "l":
-            LongBreak(l)
+            pom.LongBreak(l)
         }
-    }
 
-    Notify(HEADLINE, "DONE", false)
+    }
+}
+
+func main() {
+    flag.Parse()
+
+    p := &Pomodoro{}
+
+    p.PrintHeader()
+    p.Run()
+
+    p.Notify(HEADLINE, "Great job!", false)
 }
